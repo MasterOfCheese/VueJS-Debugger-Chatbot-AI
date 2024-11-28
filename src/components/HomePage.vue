aadd3fa721dd355ca1dc9b5a0629d4be63cd9cad
DuyHoang <pnam24823@gmail.com>
2024-11-28 17:25:48 +0700

    test1

---
+++ src/components/HomePage.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+      <h1>Home Page</h1>
+    </div>
+  </template>
+  <style scoped>
+  h1 {
+    display: flex;
+    justify-content: center;
+    align-content: center;
+  }
+  </style>
