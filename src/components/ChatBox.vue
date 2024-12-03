f946394d4cc1ca0b98eaa0028c28b51e33bc7dc0
DuyHoang <pnam24823@gmail.com>
2024-12-03 17:24:05 +0700

    1st_hit

---
+++ src/components/ChatBox.vue
@@ -0,0 +1,113 @@
+<template>
+    <div class="chat-box">
+      <div class="input-container">
+        <input
+          v-model="message"
+          type="text"
+          class="message-input"
+          placeholder="Message FoxconnGPT :))"
+        />
+        <div class="buttons-container">
+          <button class="send-button" @click="sendChats"></button>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script setup lang="js">
+  import { ref } from "vue";
+  // import { sendAllMessages } from "@/service";
+  // import { CHATS } from "@/stores/chat";
+  
+  const message = ref("");
+
+  
+  // async function sendChats() {
+  //   const userMessage = {
+  //     role: "user",
+  //     content: message.value,
+  //   };
+  //   CHATS.value.push(userMessage);
+  
+  //   let chatGPTMessage = await sendAllMessages(CHATS.value);
+  //   CHATS.value.push(chatGPTMessage);
+  // }
+  </script>
+  
+  <style lang="scss" scoped>
+  @keyframes toRightFromLeft {
+    49% {
+    transform: translateX(100%);
+    }
+    50% {
+    opacitX: 0;
+    transform: translateX(-100%);
+    }
+    51% {
+    opacity: 1;
+    }
+  }
+  .chat-box {
+    display: flex;
+    gap: 10px;
+    box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+    position: absolute;
+    width: 45%;
+    bottom: 12%;
+    left: 50%;
+    transform: translateX(-50%);
+  }
+  
+  .input-container {
+    display: flex;
+    flex: 1;
+    position: relative;
+    border: 2px solid #d1d1d1;
+    padding: 10px;
+    border-radius: 8px;
+  }
+  
+  .message-input {
+    width: 100%;
+    border: none;
+    outline: none;
+  }
+  
+  .buttons-container {
+    display: flex;
+    gap: 10px;
+    &:hover, &:active, &:focus {
+      .send-button {
+        background-color: #696161;
+        transition: all 0.35s ease-in-out;
+        &::after {
+          animation: toRightFromLeft 0.33s ease-in-out forwards;
+        }
+      }
+    }
+  }
+  
+  .send-button {
+    border: 1px solid #d1d1d1;
+    border-radius: 8px;
+    cursor: pointer;
+    background-color: #007bff;
+    transition: all 0.35s ease-in-out;
+    color: white;
+    position: relative;
+    padding: 1.2em 1.3em;
+    overflow: hidden;
+      &::after {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        content: '➤';
+        top: 0;
+        left: 0;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      }
+  }
+  </style>
+  
---
+++ src/components/ChatsContainer.vue
@@ -0,0 +1,64 @@
+<template>
+    <div>
+      <Message
+        v-for="(chat, index) in CHATS"
+        :key="index"
+        :content="chat.content"
+        :role="chat.role"
+      />
+      <div class="nameBot">Foxconn ChatBot</div>
+      <div class="suggest">
+        <div class="suggest-area"><p id="suggest1">Regarding Technology Features</p></div>
+        <div class="suggest-area"><p id="suggest2">Regarding Technology Features</p></div>
+        <div class="suggest-area"><p id="suggest3">Regarding Technology Features</p></div>
+        <div class="suggest-area"><p id="suggest4">Regarding Technology Features</p></div>
+      </div>
+    </div>
+  </template>
+  
+  <script setup lang="js">
+  import { onMounted, ref } from "vue";
+//   import MessageBox from "./MessageBox.vue";
+  // import { CHATS } from "@/stores/chat";
+  
+  const chatsContainer = ref(null);
+  
+  onMounted(() => {
+    chatsContainer.value?.scrollTo(0, chatsContainer.value.scrollHeight);
+  });
+  </script>
+  
+  <style scoped>
+  .chats-container {
+    max-height: 80vh;
+    overflow-y: auto;
+  }
+  p#suggest1, p#suggest2, p#suggest3, p#suggest4 {
+    background-color: #F9F9F9;
+    width: max-content;
+    font-size: smaller;
+    padding: 1.3em 3em;
+    cursor: pointer;
+    border-radius: 8px;
+}
+.suggest {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 1%;
+}
+.suggest-area {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.nameBot {
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 20px;
+    font-weight: 500;
+    margin-bottom: 50px;
+}
+  </style>
+  
