9dfc7b200ac65c0c25102f9a8119322b2858ddec
DuyHoang <pnam24823@gmail.com>
2024-12-16 17:00:29 +0700

    amen

--- src/main.js
+++ src/main.js
@@ -2,12 +2,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { createPinia } from 'pinia'
 import vuetify from './plugins/vuetify'
-import '@mdi/font/css/materialdesignicons.css';
+import '@mdi/font/css/materialdesignicons.css'
+import router from './router'
 
 
 const pinia = createPinia()
 const app = createApp(App)
 
+app.use(router)
 app.use(pinia)
 app.use(vuetify)
 
