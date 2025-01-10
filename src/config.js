484999b8f5ba87c0e0c4440bc16284d801a026a9
DuyHoang <pnam24823@gmail.com>
2025-01-10 10:49:09 +0700

    Remove config.js from tracking

--- src/config.js
+++
@@ -1,7 +0,0 @@
-// file này để xử lý riêng cho stores/config.js để tạo 1 dynamic variable cấu hình cho cái ip trong các API
-// nó cũng tương tự như public/config.json thôi 
-// nhưng để xử lý riêng cho pinia thì ko dùng dc API_BASE_URL trong public/config.json nên phải làm như này
-export const config = {
-    API_BASE_URL: 'http://172.20.10.4:5000',
-  };
-  
